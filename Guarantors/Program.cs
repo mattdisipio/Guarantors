fb033f6d62d14b1b53501328bb073bc9cf58b315
Matt DiSipio <mattdisipio@gmail.com>
2018-10-23 20:57:59 -0400

    Simple solution for posting json array of ints

---
+++ Guarantors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Guarantors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWork();
+
+            Console.ReadLine();
+           
+        }
+
+        private static async void DoWork()
+        {
+            var client = new HttpClient();
+
+
+            var json = "{\"numbers\":[3,2,1]}";
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            
+            var response = await client.PostAsync("https://research.theguarantors.com/qa-test/ ", content);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(responseString);
+        }
+    }
+}
